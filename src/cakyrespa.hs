f0834dc034926c43233f4af3728e248de75f339f
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-10-01 17:44:04 +0900

    add colors

--- src/cakyrespa.hs
+++ src/cakyrespa.hs
@@ -66,7 +66,32 @@ readPilno s = do
 
 skaste :: [(String, (Int, Int, Int))]
 skaste = [
-	("xunre", (255, 0, 0))
+	("xekri", (0, 0, 0)),
+	("blabi", (255, 255, 255)),
+
+	("xunre", (255, 0, 0)),
+	("labri'o", (0, 255, 0)),	-- Lime
+	("crinyblabi", (0, 255, 0)),	-- Lime
+	("blanu", (0, 0, 255)),
+
+	("pelxu", (255, 255, 0)),
+	("cicna", (0, 255, 255)),
+	("nukni", (255, 0, 255)),
+
+	("rijyska", (192, 192, 192)),	-- Silver
+	("grusi", (128, 128, 128)),
+
+--	("?", (128, 0, 0)),		-- Maroon
+	("alzaityska", (128, 128, 0)),	-- Olive
+	("crino", (0, 128, 0)),
+	("zirpu", (128, 0, 128)),	-- Purple
+--	("?", (0, 128, 128)),		-- Teal
+--	("?", (0, 0, 128)),		-- Navy
+
+	("bunre", (165, 42, 42)),	-- Brown
+	("narju", (255, 165, 0)),	-- Orange
+	("sloska", (255, 215, 0)),	-- Gold
+	("xunblabi", (255, 192, 203))	-- Pink
  ]
 
 readLAhU2 :: [(Tag, Sumti)] -> Maybe Double
