7116225c02a59d298a73496173db29778e64ff37
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-10-08 07:42:28 +0900

    not use unsafe

--- src/Run.hs
+++ src/Run.hs
@@ -10,70 +10,67 @@ import Graphics.UI.GLUT.Turtle(
 	penup, pendown, hideturtle, showturtle, shapesize,
 	pensize, pencolor, fillcolor, bgcolor, windowWidth, windowHeight)
 
-import System.IO.Unsafe(unsafePerformIO)
 import Control.Applicative((<$>))
 import Control.Monad(replicateM_)
-import Data.IORef(IORef, newIORef, readIORef)
+import Data.IORef(IORef, readIORef)
 import Data.IORef.Tools(atomicModifyIORef_)
 
 --------------------------------------------------------------------------------
 
-theTable :: IORef [(String, [Sumti] -> Command)]
-theTable = unsafePerformIO $ newIORef []
-writeTable :: String -> ([Sumti] -> Command) -> IO ()
-writeTable cmene fasnu = atomicModifyIORef_ theTable ((cmene, fasnu) :)
-readTable :: String -> IO (Maybe ([Sumti] -> Command))
-readTable cmene = lookup cmene <$> readIORef theTable
+writeT :: a -> b -> IORef [(a, b)] -> IO ()
+writeT k v t = atomicModifyIORef_ t ((k, v) :)
+readT :: Eq a => a -> IORef [(a, b)] -> IO (Maybe b)
+readT k t = lookup k <$> readIORef t
 
-run :: Field -> Turtle -> Command -> IO Bool
-run _ t (KLAMA x y) = goto t x y >> return True
-run _ t (CRAKLA d) = forward t d >> return True
-run _ t (RIXYKLA d) = backward t d >> return True
-run _ t (ZUNLE d) = left t d >> return True
-run _ t (PRITU d) = right t d >> return True
-run _ t (PEBYCISNI s) = pensize t s >> return True
-run _ t (PEBYSKA r g b) = pencolor t (r, g, b) >> return True
-run _ t (BURSKA r g b) = fillcolor t (r, g, b) >> return True
-run _ t (FLOSKA r g b) = bgcolor t (r, g, b) >> return True
-run _ t COhACLUGAU = beginfill t >> return True
-run _ t COhUCLUGAU = endfill t >> return True
-run _ t XRUTI = undo t >> return True
-run _ t (CISNI s) = shapesize t s s >> return True
-run _ t NAPILNOLOPENBI = penup t >> return True
-run _ t PILNOLOPENBI = pendown t >> return True
-run _ t NAVISKA = hideturtle t >> return True
-run _ t VISKA = showturtle t >> return True
-run _ _ COhO = return False
-run f t (Commands c d) = run f t c >> run f t d
-run f t (CommandList cl) =
-	mapM_ (run f t) cl >> return True
-run f t (Repeat n c) = replicateM_ n (run f t c) >>
+run :: Field -> Turtle -> IORef [(String, [Sumti] -> Command)] -> Command -> IO Bool
+run _ t _ (KLAMA x y) = goto t x y >> return True
+run _ t _ (CRAKLA d) = forward t d >> return True
+run _ t _ (RIXYKLA d) = backward t d >> return True
+run _ t _ (ZUNLE d) = left t d >> return True
+run _ t _ (PRITU d) = right t d >> return True
+run _ t _ (PEBYCISNI s) = pensize t s >> return True
+run _ t _ (PEBYSKA r g b) = pencolor t (r, g, b) >> return True
+run _ t _ (BURSKA r g b) = fillcolor t (r, g, b) >> return True
+run _ t _ (FLOSKA r g b) = bgcolor t (r, g, b) >> return True
+run _ t _ COhACLUGAU = beginfill t >> return True
+run _ t _ COhUCLUGAU = endfill t >> return True
+run _ t _ XRUTI = undo t >> return True
+run _ t _ (CISNI s) = shapesize t s s >> return True
+run _ t _ NAPILNOLOPENBI = penup t >> return True
+run _ t _ PILNOLOPENBI = pendown t >> return True
+run _ t _ NAVISKA = hideturtle t >> return True
+run _ t _ VISKA = showturtle t >> return True
+run _ _ _ COhO = return False
+run f t tbl (Commands c d) = run f t tbl c >> run f t tbl d
+run f t tbl (CommandList cl) =
+	mapM_ (run f t tbl) cl >> return True
+run f t tbl (Repeat n c) = replicateM_ n (run f t tbl c) >>
 	return True
-run _ _ (MORJI cmene fasnu) = writeTable cmene fasnu >> return True
-run f t (GASNU cmene sumti) = do
-	mfasnu <- readTable cmene
-	flip (maybe $ run f t $ ErrorC $ "not defined: " ++ cmene) mfasnu $
-		\fasnu -> run f t $ fasnu sumti
-run _ t (SAVEASSVG fp) = do
+run _ _ tbl (MORJI cmene fasnu) = writeT cmene fasnu tbl >> return True
+run f t tbl (GASNU cmene sumti) = do
+	mfasnu <- readT cmene tbl
+	flip (maybe $ run f t tbl $ ErrorC $ "not defined: " ++ cmene) mfasnu $
+		\fasnu -> run f t tbl $ fasnu sumti
+run _ t _ (SAVEASSVG fp) = do
 	w <- windowWidth t
 	h <- windowHeight t
 	svg <- getSVG t
 	writeFile fp $ showSVG w h svg
 	return True
-run _ t (SAVEASCAK fp) = inputs t >>= writeFile fp . show >> return True
-run _ t (READFILE fp) = readFile fp >>= runInputs t . read >> return True
-run _ _ KUNTI = return True
-run f _ (Unknown u) = do
+run _ t _ (SAVEASCAK fp) = inputs t >>= writeFile fp . show >> return True
+run _ t _ (READFILE fp) = readFile fp >>= runInputs t . read >> return True
+run _ _ _ KUNTI = return True
+run f _ _ (Unknown u) = do
 	outputString f ".i mi na jimpe"
 	putStr "Unknown " >> print u
 	return True
-run f _ (UnknownSelpli us) = do
+run f _ _ (UnknownSelpli us) = do
 	outputString f ".i mi na djuno lo bi'unai selpli"
 	putStr "Unknown " >> print us
 	return True
-run f _ ParseErrorC = do
+run f _ _ ParseErrorC = do
 	outputString f ".i di'u na drani fo lo gerna"
 	return True
-run _ _ (ErrorC str) = do
+run _ _ _ (ErrorC str) = do
 	putStrLn $ "error: " ++ str
 	return True
--- src/cakyrespa.hs
+++ src/cakyrespa.hs
@@ -8,6 +8,7 @@ import Graphics.UI.GLUT(mainLoop)
 import Graphics.UI.GLUT.Turtle(
 	initialize, openField, newTurtle, prompt, notundo, oninputtext,
 	shape, shapesize)	
+import Data.IORef(newIORef)
 
 
 --------------------------------------------------------------------------------
@@ -17,9 +18,10 @@ main = do
 	_args <- initialize
 	f <- openField "cakyrespa" 640 480
 	t <- newTurtle f
+	table <- newIORef []
 	prompt f ".i "
 	shape t "turtle"
 	shapesize t 3 3
 	notundo t
-	oninputtext f $ run f t . command . readLojban
+	oninputtext f $ run f t table . command . readLojban
 	mainLoop
