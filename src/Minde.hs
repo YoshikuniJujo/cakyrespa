f15762c9e8b6b371d9494d9666b0b106ae3db320
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-10-10 10:56:56 +0900

    refactoring Minde.hs

--- src/Minde.hs
+++ src/Minde.hs
@@ -16,11 +16,11 @@ import Data.IORef.Tools(atomicModifyIORef_)
 
 --------------------------------------------------------------------------------
 
-readT :: Eq a => a -> IORef [(a, b)] -> IO (Maybe b)
-readT k t = lookup k <$> readIORef t
+vabytcidu :: Eq a => a -> IORef [(a, b)] -> IO (Maybe b)
+vabytcidu cmene vanbi = lookup cmene <$> readIORef vanbi
 
-writeT :: a -> b -> IORef [(a, b)] -> IO ()
-writeT k v t = atomicModifyIORef_ t ((k, v) :)
+vabyciha :: a -> b -> IORef [(a, b)] -> IO ()
+vabyciha cmene fasnu vanbi = atomicModifyIORef_ vanbi ((cmene, fasnu) :)
 
 minde :: Field -> Turtle -> IORef [(String, [Sumti] -> Minde)] -> Minde -> IO Bool
 minde _ t _ (KLAMA x y) = goto t x y >> return True
@@ -28,34 +28,38 @@ minde _ t _ (CRAKLA d) = forward t d >> return True
 minde _ t _ (RIXYKLA d) = backward t d >> return True
 minde _ t _ (ZUNLE d) = left t d >> return True
 minde _ t _ (PRITU d) = right t d >> return True
+minde _ t _ COhACLUGAU = beginfill t >> return True
+minde _ t _ COhUCLUGAU = endfill t >> return True
 minde _ t _ (PEBYCISNI s) = pensize t s >> return True
 minde _ t _ (PEBYSKA r g b) = pencolor t (r, g, b) >> return True
 minde _ t _ (BURSKA r g b) = fillcolor t (r, g, b) >> return True
 minde _ t _ (FLOSKA r g b) = bgcolor t (r, g, b) >> return True
-minde _ t _ COhACLUGAU = beginfill t >> return True
-minde _ t _ COhUCLUGAU = endfill t >> return True
-minde _ t _ XRUTI = undo t >> return True
-minde _ t _ (CISNI s) = shapesize t s s >> return True
 minde _ t _ NAPILNOLOPENBI = penup t >> return True
 minde _ t _ PILNOLOPENBI = pendown t >> return True
 minde _ t _ NAVISKA = hideturtle t >> return True
 minde _ t _ VISKA = showturtle t >> return True
-minde _ _ _ COhO = return False
-minde f t tbl (MIDSTE cl) = mapM_ (minde f t tbl) cl >> return True
-minde _ _ tbl (MORJI cmene fasnu) = writeT cmene fasnu tbl >> return True
-minde f t tbl (GASNU cmene sumti) = do
-	mfasnu <- readT cmene tbl
-	flip (maybe $ minde f t tbl $ SRERA $ "not defined: " ++ cmene) mfasnu $
-		\fasnu -> minde f t tbl $ fasnu sumti
+minde _ t _ (CISNI s) = shapesize t s s >> return True
+minde _ t _ XRUTI = undo t >> return True
+minde _ _ vanbi (MORJI cmene fasnu) = vabyciha cmene fasnu vanbi >> return True
+minde f t vanbi (GASNU cmene sumti) = vabytcidu cmene vanbi >>=
+	minde f t vanbi . maybe (SRERA $ "not defined: " ++ cmene) ($ sumti)
+minde _ t _ (REJGAUSETAICAK fp) = inputs t >>= writeFile fp . show >> return True
+minde _ t _ (TCIDU fp) = readFile fp >>= runInputs t . read >> return True
 minde _ t _ (REJGAUSETAISVG fp) = do
 	w <- windowWidth t
 	h <- windowHeight t
 	svg <- getSVG t
 	writeFile fp $ showSVG w h svg
 	return True
-minde _ t _ (REJGAUSETAICAK fp) = inputs t >>= writeFile fp . show >> return True
-minde _ t _ (TCIDU fp) = readFile fp >>= runInputs t . read >> return True
+minde _ _ _ COhO = return False
+minde f t vanbi (MIDSTE cl) = mapWhile (minde f t vanbi) cl
 minde f _ _ (SRERA str) = do
 	outputString f ".i mi na jimpe"
 	putStrLn $ "error: " ++ str
 	return True
+
+mapWhile :: Monad m => (a -> m Bool) -> [a] -> m Bool
+mapWhile _ [] = return True
+mapWhile f (x : xs) = do
+	b <- f x
+	if b then mapWhile f xs else return False
