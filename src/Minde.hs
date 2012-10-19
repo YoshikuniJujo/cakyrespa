ef860ce48857745667ef32e171c8feeb5994d7cb
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-10-19 16:08:27 +0900

    for new version gluturtle

--- src/Minde.hs
+++ src/Minde.hs
@@ -3,8 +3,8 @@ module Minde (minde) where
 import Klesi(Minde(..), Sumti)
 
 import Text.XML.YJSVG(showSVG)
-import Graphics.UI.GLUT.Turtle(setFieldSize,
-	Field, outputString,
+import Graphics.UI.GLUT.Turtle(setFieldSize, Console,
+	Field, consoleOutput,
 	Turtle, runInputs, inputs, getSVG,
 	goto, forward, backward, left, right, beginfill, endfill, undo,
 	penup, pendown, hideturtle, showturtle, shapesize,
@@ -22,42 +22,42 @@ vabytcidu cmene vanbi = lookup cmene <$> readIORef vanbi
 vabyciha :: a -> b -> IORef [(a, b)] -> IO ()
 vabyciha cmene fasnu vanbi = atomicModifyIORef_ vanbi ((cmene, fasnu) :)
 
-minde :: Field -> Turtle -> IORef [(String, [Sumti] -> Minde)] -> Minde -> IO Bool
-minde _ t _ (KLAMA x y) = goto t x y >> return True
-minde _ t _ (CRAKLA d) = forward t d >> return True
-minde _ t _ (RIXYKLA d) = backward t d >> return True
-minde _ t _ (ZUNLE d) = left t d >> return True
-minde _ t _ (PRITU d) = right t d >> return True
-minde _ t _ COhACLUGAU = beginfill t >> return True
-minde _ t _ COhUCLUGAU = endfill t >> return True
-minde _ t _ (PEBYCISNI s) = pensize t s >> return True
-minde _ t _ (PEBYSKA r g b) = pencolor t (r, g, b) >> return True
-minde _ t _ (BURSKA r g b) = fillcolor t (r, g, b) >> return True
-minde _ t _ (FLOSKA r g b) = bgcolor t (r, g, b) >> return True
-minde _ t _ NAPILNOLOPENBI = penup t >> return True
-minde _ t _ PILNOLOPENBI = pendown t >> return True
-minde _ t _ NAVISKA = hideturtle t >> return True
-minde _ t _ VISKA = showturtle t >> return True
-minde _ t _ (CISNI s) = shapesize t s s >> return True
-minde f _ _ (CISNYGAUFOLDI w h) = setFieldSize f w h >> return True
-minde _ t _ XRUTI = undo t >> return True
-minde _ _ vanbi (MORJI cmene fasnu) = vabyciha cmene fasnu vanbi >> return True
-minde f t vanbi (GASNU cmene sumti) = vabytcidu cmene vanbi >>=
-	minde f t vanbi . maybe (SRERA $ "not defined: " ++ cmene) ($ sumti)
-minde _ t _ (REJGAUSETAICAK fp) = inputs t >>= writeFile fp . show >> return True
-minde _ t _ (TCIDU fp) = readFile fp >>= runInputs t . read >> return True
-minde _ t _ (REJGAUSETAISVG fp) = do
+minde :: Field -> Console -> Turtle -> IORef [(String, [Sumti] -> Minde)] -> Minde -> IO Bool
+minde _ _ t _ (KLAMA x y) = goto t x y >> return True
+minde _ _ t _ (CRAKLA d) = forward t d >> return True
+minde _ _ t _ (RIXYKLA d) = backward t d >> return True
+minde _ _ t _ (ZUNLE d) = left t d >> return True
+minde _ _ t _ (PRITU d) = right t d >> return True
+minde _ _ t _ COhACLUGAU = beginfill t >> return True
+minde _ _ t _ COhUCLUGAU = endfill t >> return True
+minde _ _ t _ (PEBYCISNI s) = pensize t s >> return True
+minde _ _ t _ (PEBYSKA r g b) = pencolor t (r, g, b) >> return True
+minde _ _ t _ (BURSKA r g b) = fillcolor t (r, g, b) >> return True
+minde _ _ t _ (FLOSKA r g b) = bgcolor t (r, g, b) >> return True
+minde _ _ t _ NAPILNOLOPENBI = penup t >> return True
+minde _ _ t _ PILNOLOPENBI = pendown t >> return True
+minde _ _ t _ NAVISKA = hideturtle t >> return True
+minde _ _ t _ VISKA = showturtle t >> return True
+minde _ _ t _ (CISNI s) = shapesize t s s >> return True
+minde f _ _ _ (CISNYGAUFOLDI w h) = setFieldSize f w h >> return True
+minde _ _ t _ XRUTI = undo t >> return True
+minde _ _ _ vanbi (MORJI cmene fasnu) = vabyciha cmene fasnu vanbi >> return True
+minde f c t vanbi (GASNU cmene sumti) = vabytcidu cmene vanbi >>=
+	minde f c t vanbi . maybe (SRERA $ "not defined: " ++ cmene) ($ sumti)
+minde _ _ t _ (REJGAUSETAICAK fp) = inputs t >>= writeFile fp . show >> return True
+minde _ _ t _ (TCIDU fp) = readFile fp >>= runInputs t . read >> return True
+minde _ _ t _ (REJGAUSETAISVG fp) = do
 	w <- windowWidth t
 	h <- windowHeight t
 	svg <- getSVG t
 	writeFile fp $ showSVG w h svg
 	return True
-minde _ _ _ (REJGAUSETAIJBO _) = return True
-minde _ _ _ (TCIDUSETAIJBO _) = return True
-minde _ _ _ COhO = return False
-minde f t vanbi (MIDYSTE cl) = mapWhile (minde f t vanbi) cl
-minde f _ _ (SRERA str) = do
-	outputString f ".i mi na jimpe"
+minde _ _ _ _ (REJGAUSETAIJBO _) = return True
+minde _ _ _ _ (TCIDUSETAIJBO _) = return True
+minde _ _ _ _ COhO = return False
+minde f c t vanbi (MIDYSTE cl) = mapWhile (minde f c t vanbi) cl
+minde _ c _ _ (SRERA str) = do
+	consoleOutput c ".i mi na jimpe"
 	putStrLn $ "error: " ++ str
 	return True
 
--- src/cakyrespa.hs
+++ src/cakyrespa.hs
@@ -7,7 +7,8 @@ import Klesi(Text(ParseError), Minde(REJGAUSETAIJBO, TCIDUSETAIJBO))
 
 import Graphics.UI.GLUT(mainLoop)
 import Graphics.UI.GLUT.Turtle(
-	initialize, openField, openConsole, setConsole, prompt, oninputtext,
+	initialize, openField, openConsole, setConsole, consolePrompt,
+	oncommand,
 	newTurtle, notundo, shape, shapesize)	
 import Data.IORef(newIORef, readIORef)
 import Data.IORef.Tools(atomicModifyIORef_)
@@ -24,20 +25,20 @@ main = do
 	t <- newTurtle f
 	env <- newIORef []
 	cmds <- newIORef []
-	prompt f ".i "
+	consolePrompt c ".i "
 	shape t "turtle"
 	shapesize t 3 3
 	notundo t
-	oninputtext f $ \cmd -> do
+	oncommand f $ \cmd -> do
 		let	p = parse cmd
 			j = jimpe p
 		case (p, j) of
 			(_, REJGAUSETAIJBO fp) ->
 				writeFile fp . unlines . reverse =<< readIORef cmds
 			(_, TCIDUSETAIJBO fp) ->
-				readFile fp >>= minde f t env . jimpe . parse
+				readFile fp >>= minde f c t env . jimpe . parse
 					>> return ()
 			(ParseError _, _) -> return ()
 			_ -> atomicModifyIORef_ cmds ((".i" ++ cmd) :)
-		minde f t env j
+		minde f c t env j
 	mainLoop
